adae0f5873fe4f884459fa98b7b27fcca4926164
Lucasggk <lucasemanuelguimaraes20@gmail.com>
2025-04-30 16:15:16 -0300

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,1 +1,15 @@
+local Fluent = loadstring(Game:HttpGet("https://raw.githubusercontent.com/discoart/FluentPlus/refs/heads/main/release.lua", true))()
+
+local Window = Fluent:CreateWindow({
+    Title = "Blox fruits",
+    SubTitle = "Feito por Lucas",
+    TabWidth = 160,
+    Size = UDim2.fromOffset(580, 460),
+    Acrylic = false,
+    Theme = "Dark"
+})
+
+local main = Window:AddTab({ Title = "main farm", Icon = "home" })
+local config = Window:AddTab({ Title = "configurações", Icon = "setting" })
+
 
